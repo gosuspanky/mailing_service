500a25bebceb4db0cc6e0f63d41cd924002da462
Artur Gaetov <agaetov@gmail.com>
2024-06-12 00:11:32 +0300

    bug_fix#5

--- mail_messages/models.py
+++ mail_messages/models.py
@@ -7,7 +7,7 @@ class Message(models.Model):
     message_subject = models.CharField(max_length=100, verbose_name="Тема письма")
     text = models.TextField(verbose_name="Текст письма")
 
-    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, **NULLABLE)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, **NULLABLE, related_name='messages')
 
     def __str__(self):
         return self.message_subject
--- mailing/services.py
+++ mailing/services.py
@@ -1,13 +1,14 @@
 import smtplib
 from django.core.mail import send_mail
 
-from config import settings
 from config.settings import EMAIL_HOST_USER
 import pytz
 from datetime import datetime
 from django.core.cache import cache
 from mailing.models import Mailing, Log
 from blog.models import Article
+from config import settings
+
 
 MY_TIME_ZONE = pytz.timezone(settings.TIME_ZONE)
 NOW = datetime.now(MY_TIME_ZONE)
@@ -15,9 +16,9 @@ NOW = datetime.now(MY_TIME_ZONE)
 
 def send_mail_func(mailing):
     """Отправляет письмо на почту клиентам из рассылки, записывает попытки рассылки"""
-    customer_emails = mailing.clients.values_list("email", flat=True)
-    subject = mailing.message.subject
-    text_of_message = mailing.message.text
+    customer_emails = mailing.customers.values_list('email', flat=True)
+    subject = mailing.messages.message_subject
+    text_of_message = mailing.messages.text
     try:
         send_response = send_mail(
             subject=subject,
@@ -26,53 +27,34 @@ def send_mail_func(mailing):
             recipient_list=customer_emails,
             fail_silently=False,
         )
-
         for customer in mailing.customers.all():
-            Log.objects.create(
-                try_time=NOW,
-                try_status=Log.SUCCESS,
-                server_answer=send_response,
-                mailing=mailing,
-                customers=customer,
-            )
+            Log.objects.create(try_time=NOW, try_status=Log.SUCCESS, server_answer=send_response,
+                              mailing=mailing, customers=customer)
         return send_response
-
     except smtplib.SMTPException as e:
         for customer in mailing.customers.all():
-            Log.objects.create(
-                try_time=NOW,
-                try_status=Log.FAIL,
-                server_answer=e,
-                mailing=mailing,
-                customers=customer,
-            )
-
+            Log.objects.create(try_time=NOW, try_status=Log.FAIL, server_answer=e,
+                              mailing=mailing, customers=customer)
 
 def send_mails():
     """Запускает рассылки, меняет их статусы, проверяет периодичность"""
-    mailings = (
-        Mailing.objects.filter(status__in=["created", "started"])
-        .filter(datetime_start__lte=NOW)
-        .prefetch_related("clients")
-        .select_related("message")
-    )
+    mailings = (Mailing.objects.filter(status__in=['created', 'started']).filter(datetime_start__lte=NOW).
+                prefetch_related('customers').select_related('messages'))
 
     for mailing in mailings:
-        print("прошел по рассылке")
+        print('прошел по рассылке')
 
         if mailing.datetime_finish < NOW:
             mailing.status = Mailing.FINISHED
             mailing.save()
-            print("отработал статус FINISHED")
-
+            print('отработал статус FINISHED')
         elif mailing.status == Mailing.CREATED:
             send_mail_func(mailing)
             mailing.status = Mailing.STARTED
             mailing.save()
-            print("Отработала отправка и смена статуса на STARTED")
-
+            print('Отработала отправка и смена статуса на STARTED')
         elif mailing.status == Mailing.STARTED:
-            try_time = Log.objects.filter(mailing=mailing).order_by("try_time").first()
+            try_time = Log.objects.filter(mailing=mailing).order_by('try_time').first()
             if try_time:
                 delta = NOW - try_time.try_time
                 if mailing.period == Mailing.DAILY and delta.days >= 1:
@@ -81,15 +63,15 @@ def send_mails():
                     send_mail_func(mailing)
                 elif mailing.period == Mailing.MONTHLY and delta.days >= 30:
                     send_mail_func(mailing)
-                print("Отработала отправка рассылки со статусом Запущена")
+                print('Отработала отправка рассылки со статусом Запущена')
 
-    print(f"Текущее время:{NOW}")
+    print(f'Текущее время:{NOW}')
 
 
 def get_cashed_article_list():
     """Функция возвращает закешированный список статей"""
 
-    key = "articles"
+    key = 'articles'
     article_list = Article.objects.all()
 
     if settings.CACHE_ENABLED:
